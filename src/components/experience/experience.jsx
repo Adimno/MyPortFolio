5551d3e67f662301050358754eb65dc44427491b
Jayjay Naval <jayjaynaval02@gmail.com>
2023-01-13 17:34:26 +0800

    updated

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,19 +1,107 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const experience = () => {
   return (
     <section id="experience">
     <h5> What Skills I have</h5>
     <h2> My Experience</h2>
-     <div className="container experience_containter">
-
+     <div className="container experience_container">
+ 
       <div className="experience_frontend">
+        <h3> Frontend Development</h3>
+     <div className="div experience_content">
+       <article className="experience_details">
+         <BsPatchCheckFill className='experience_details-icon'/>
+    
+        <div>
+        <h4> HTML</h4>
+         <small className='text-light'> Experienced</small>
+        </div>
 
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill className='experience_details-icon' />
+         <div>
+         <h4> CSS</h4>
+         <small className='text-light'> Intermediate</small>
+         </div>
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill className='experience_details-icon'/>
+          <div>
+         <h4> Javascript</h4>
+         <small className='text-light'> Experienced</small>
+       </div>
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill className='experience_details-icon'/>
+         <div>
+         <h4> Bootstrap</h4>
+         <small className='text-light'> Experienced</small>
+         </div>
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill className='experience_details-icon' />
+         <div>
+         <h4> Tailwind</h4>
+         <small className='text-light'> Experienced</small>
+         </div>
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill  className='experience_details-icon'/>
+         <div>
+         <h4> React</h4>
+         <small className='text-light'> Experienced</small>
+         </div>
+         </article>
+      </div>
       </div>
 
+      {/* END OF FRONTEND */}
       <div className="experience_backend">
-        
+      <h3> Backend Development</h3>
+     <div className="div experience_content">
+       <article className="experience_details">
+         <BsPatchCheckFill  className='experience_details-icon'/>
+            <div>
+            <h4> PHP</h4>
+         <small className='text-light'> Intermediate</small>
+            </div>
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill  className='experience_details-icon'/>
+          <div>
+          <h4> Firebase</h4>
+         <small className='text-light'> Basic</small>
+          </div>
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill className='experience_details-icon'/>
+          <div>
+          <h4> MySQL</h4>
+         <small className='text-light'> Experienced</small>
+          </div>
+         </article>
+
+         <article className="experience_details">
+         <BsPatchCheckFill className='experience_details-icon'/>
+          <div>
+          <h4> Python</h4>
+         <small className='text-light'> Basic</small>
+          </div>
+         </article>
+
+       
+      </div>
       </div>
      </div>
     </section>
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import './portfolio.css'
+import {ME} from '../../assets/me.jpg'
+
 const portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5> My Recent Work </h5>
+      <h2> Portfolio</h2>
+      
+      <div className="container portfolio_container">
+     <article className='portfolio_item'>
+       <div className="portfolio_item-image">
+        <h3> This is a portfolio item title</h3>
+        <a href="https://github.com" className='btn' target='_blank'>Github</a>
+        <a href="https://dribbble.com/search/alien" className='btn btn-primary' target='_blank'>Live Demo</a>
+       </div>
+     </article>
+
+
+      </div>
+
+    
+
+    </section>
   )
 }
 
